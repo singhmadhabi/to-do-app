4955b131b4f46160c644b54f7ad41a5a156c406e
Madhabi Singh <singhmadhabi4@gmail.com>
2023-09-07 06:44:39 +0545

    database(seed.js)

--- routes/routes.api.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const todoRouter = require("../modules/todos/todo.api");
-const subtaskRouter = require("../modules/subtasks/subtask.api");
-
-router.use("/todos", todoRouter);
-router.use("/subtasks", subtaskRouter);
-
-module.exports = router;
